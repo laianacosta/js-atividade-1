80798490653e905d146a9f18c2561ebae15a8ab9
laianacosta <laanacosta2003@gmail.com>
2021-05-17 17:27:46 -0300

    implementações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-alert('Olá!');
+const original = document.querySelector('#original')
+const contador = document.querySelector('#contador')
+const select = document.querySelector('#efeito')
+const modificado = document.querySelector('#modificado')
+
+
+original.addEventListener('keyup',function() {
+  console.log(original.value.length)
+  contador.textContent = 0 + original.value.length
+ 
+ 
+ if (efeito.value == 'uppercase') {
+   modificado.value = original.value.toUpperCase()
+ }
+ else {
+   modificado.value = original.value.toLowerCase()
+ }
+ 
+})
